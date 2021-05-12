39bfe26a07680aa0e17544312ba0fe6e1d6d759a
Sameer <sameer@umd.edu>
2021-05-12 11:31:53 -0400

    added createdRandomRobots function

--- networkDelay.py
+++ networkDelay.py
@@ -1,2 +1,30 @@
+import random
+
+class Graph:
+
+    def __init__(self):
+        self.graph = [[0 for _ in range(10)] for _ in range(10)]
+
+    def createRandomRobots(self, K):
+
+        """
+        Description: Creates K random robot in self.graph
+        """
+        robotsCreated = 0
+        while robotsCreated < K :
+            i = random.randint(0, 9))
+            j = random.randint(0, 9))
+            if self.graph[i][j] != 1:
+                self.graph[i][j] = 1
+                robotsCreated += 1
+        
+    def determineNetworkDelay(self):
+        
+
+
+
+
+graph = Graph()
+
 
 
